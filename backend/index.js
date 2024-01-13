c064afc674dd148c99579f5224470cae4511c0a8
zan <akasizan@gmail.com>
2024-01-13 20:40:26 +0100

    prepared artist registration backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-
+const DB = require("./db/dbConn.js");
 const port = 8199;
+const users = require("./routes/users.js");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("");
+app.use("/users", users);
+
+app.get("/", (req, res, next) => {
+  res.send("What the fuck");
 });
 
 ///App listening on port
