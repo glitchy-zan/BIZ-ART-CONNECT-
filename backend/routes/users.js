948a02c8d9af8a90c5585940e02dae14e67eae0b
zan <akasizan@gmail.com>
2024-01-15 18:10:41 +0100

    backend logic for inserting business

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,4 +21,22 @@ usersRouter.post("/artistRegister", async (req, res, next) => {
   }
 });
 
+// return JSON with error indicator
+usersRouter.post("/businessRegister", async (req, res, next) => {
+  try {
+    const business = req.body;
+    var queryResult = await DB.businessCreate(business);
+    console.log("no error!");
+    res.send({
+      error: false,
+    });
+  } catch (err) {
+    console.log("error happened!");
+    res.send({
+      error: true,
+      error_detail: err,
+    });
+  }
+});
+
 module.exports = usersRouter;
